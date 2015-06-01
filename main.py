50854d3bee2973e13b889016eb1ac2c7cd16d08a
katejurkowska <kate.jurkowska@gmail.com>
2015-06-01 20:28:55 +0200

    Utworz funkcję sum_limit

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ def fib_numbers():
     while True:
         yield b
         a, b = b, a + b
+
+
+def sum_limit(numbers, limit):
+    current_sum = 0
+    for number in numbers:
+        if current_sum + number > limit:
+            break
+        else:
+            current_sum += number
+    return current_sum
